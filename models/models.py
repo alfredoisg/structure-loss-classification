9331e0c1080746022e3ab5821142af2ba5e23858
alfredo <alfredo.sanchez@sintef.no>
2024-02-12 12:46:53 +0100

    small changes to models and reqs. added test notebook

--- models/models.py
+++ models/models.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 
+## output_size = (input_size - kernel_size + 2*padding)/stride + 1
 
 class LeNet5(nn.Module):
     def __init__(self) -> None:
@@ -15,15 +16,16 @@ class LeNet5(nn.Module):
                     padding=0,
                 ),
                 nn.BatchNorm2d(num_features=out_channels),
+                nn.ReLU(),
             )
             return layer
 
         self.convStack = nn.Sequential(
-            conv_layer(3, 6),
-            nn.MaxPool2d(2, 2),  ## 244x244 --> 122x122
-            conv_layer(6, 16),
-            nn.MaxPool2d(2, 2),  ## 122x122 --> 61x61
-            conv_layer(16, 120),
+            conv_layer(3, 6), ## 244x244 --> (244 - 5 + 2*0)/1 +1 = 240x240
+            nn.MaxPool2d(2, 2), ## 240/2 --> 120x120
+            conv_layer(6, 16), ## 120x120 --> (120 -5 + 2*0)/1 +1 = 116x116
+            nn.MaxPool2d(2, 2), ## 116/2 --> 58x58
+            conv_layer(16, 120) ## 58x58 --> (58 -5 + 2*0)/1 +1 = 54x54
         )
 
         self.fcStack = nn.Sequential(
@@ -32,9 +34,6 @@ class LeNet5(nn.Module):
             nn.Dropout(p=0.5),
             nn.ReLU(),
             nn.Linear(84, 3),
-            # nn.Dropout(p=0.5),
-            # nn.ReLU(),
-            # nn.Linear(10, 3)
         )
 
     def forward(self, x):
