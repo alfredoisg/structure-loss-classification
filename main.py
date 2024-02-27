53cff27668ec1ad3b2e2751a20b43437de9aad29
alfredoisg <alfredo.sanchez@sintef.no>
2024-02-27 09:20:36 +0100

    more testing available

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from lightning_modules.lightning_modules import LitLeNet5
 from train.train import get_features
+from utils.utils import load_targets
 
 model = LitLeNet5(num_classes=3, learning_rate=0.001, size_layer_1=10, size_layer_2=5)
 
+targets = load_targets()
 print(model)
