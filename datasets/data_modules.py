ce7bc468c5dfa4485b0ff664ea06cc1a1fd07272
alfredo <alfredo.sanchez@sintef.no>
2024-03-23 18:51:54 +0100

    best with external train val dataset, so that it can run both cv and normal

--- datasets/data_modules.py
+++ datasets/data_modules.py
@@ -1,36 +1,16 @@
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader
 import os
 import pytorch_lightning as pl
-from sklearn.model_selection import train_test_split
-from datasets.datasets import CustomDatasetWrapper
+
 
 class CustomImageDataModule(pl.LightningDataModule):
-    def __init__(self, dataset: CustomDatasetWrapper, batch_size: int = 8, num_workers: int = os.cpu_count()):
+    def __init__(self, train_dataset, val_dataset, batch_size=64, num_workers=os.cpu_count()):
         super().__init__()
-        
-        self.dataset = dataset
         self.batch_size = batch_size
         self.num_workers = num_workers
-        
-    
-    def setup(self, stage=None):
-        valid_indices = self.dataset.valid_indices
-        labels = [self.dataset.dataset.samples[i][1] for i in valid_indices]  # Extracting labels based on valid_indices
-        adjusted_labels = [self.dataset.label_map[label] for label in labels]  # Adjusting labels based on classification mode
-        
-        train_idx, val_idx = train_test_split(
-            range(len(valid_indices)),
-            test_size=0.2, 
-            stratify=adjusted_labels, 
-            random_state=42
-        )
-        
-        # Mapping back to original indices in the dataset
-        train_indices = [valid_indices[i] for i in train_idx]
-        val_indices = [valid_indices[i] for i in val_idx]
-        
-        self.train_dataset = Subset(self.dataset, train_indices)
-        self.val_dataset = Subset(self.dataset, val_indices)
+
+        self.train_dataset = train_dataset
+        self.val_dataset = val_dataset
 
     def train_dataloader(self):
         return DataLoader(
@@ -38,7 +18,7 @@ class CustomImageDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             shuffle=True,
-            pin_memory=True,
+            pin_memory=True
         )
 
     def val_dataloader(self):
@@ -47,5 +27,5 @@ class CustomImageDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             shuffle=False,
-            pin_memory=True,
+            pin_memory=True
         )
